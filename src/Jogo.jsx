cf42275d2c61676fc026c44e31e3ff1384d3d671
Tiago <tiagoalex0202@gmail.com>
2023-02-20 20:30:45 -0300

    deslocamento

--- src/Jogo.jsx
+++ src/Jogo.jsx
@@ -1,12 +1,12 @@
 import "style"
 import palavras from "./palavras"
-import imagem0 from "/home/tiagoalexandre/Documentos/projeto8-jogoforca/assets/forca0.png"
-import imagem1 from "/home/tiagoalexandre/Documentos/projeto8-jogoforca/assets/forca1.png"
-import imagem2 from "/home/tiagoalexandre/Documentos/projeto8-jogoforca/assets/forca2.png"
-import imagem3 from "/home/tiagoalexandre/Documentos/projeto8-jogoforca/assets/forca3.png"
-import imagem4 from "/home/tiagoalexandre/Documentos/projeto8-jogoforca/assets/forca4.png"
-import imagem5 from "/home/tiagoalexandre/Documentos/projeto8-jogoforca/assets/forca5.png"
-import imagem6 from "/home/tiagoalexandre/Documentos/projeto8-jogoforca/assets/forca6.png"
+import imagem0 from "./assets/forca0.png"
+import imagem1 from "./assets/forca1.png"
+import imagem2 from "./assets/forca2.png"
+import imagem3 from "./assets/forca3.png"
+import imagem4 from "./assets/forca4.png"
+import imagem5 from "./assets/forca5.png"
+import imagem6 from "./assets/forca6.png"
 
 function getRandomArbitrary(min, max) {
     return Math.random() * (max - min) + min;
