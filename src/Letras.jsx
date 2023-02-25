1c38e0ca979b744e9f5ee204645a5abf1440e60f
Tiago <tiagoalex0202@gmail.com>
2023-02-25 18:07:02 -0300

    .

--- src/Letras.jsx
+++ src/Letras.jsx
@@ -3,19 +3,25 @@ import alfabetomaiusculo from "./alfabetomaiusculo"
 
 export default function Caixas(props){
 
-    function Organizar(prop){
-        return (
-                <button onClick={() => props.clique(prop.letra)} 
-                className={props.letrasSelecionadas.length === 0 ? props.classe: "desabilitado"} 
-                data-test="letter"
-                disabled={props.letrasSelecionadas.length=== 0 ? props.desabilitado : props.letrasSelecionadas.includes(prop.letra)}>
-                    {prop.letra}</button>
-        )
-    }
-    
+    function Clique(index){
+        let indexNovo= index.toLowerCase()
+        const novoArray=[...props.letrasSelecionadas,indexNovo]
+        if(props.letrasSelecionadas.includes(indexNovo)){
+            let i=0;
+         }
+         else{
+            {props.setLetrasSelecionadas(novoArray)}
+            {props.setDesabilitado(true)}
+         }
+         console.log(novoArray)
+        }
     return(
         <div className="containerletra">
-            {alfabetomaiusculo.map((i) => (<Organizar letra={i}/> ))}
+            {alfabetomaiusculo.map((i) => ( <button onClick={() => Clique(i)} 
+                className={`botao ${props.letrasSelecionadas.includes(i)? "desabilitado" : props.classe}`}
+                data-test="letter"
+                disabled={ props.letrasSelecionadas.includes(i)}>
+                    {i}</button>))}
         </div>
     )
 }
